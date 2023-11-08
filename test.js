4fb733b085afe568c2f50b79af94b0389fe56d3f
minyuecmhe <minyuecmhe@tencent.com>
2023-11-08 17:30:35 +0800

    6.change3

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 //定义一个函数和一个url并且导出到另一个文件使用
 const baseURL = 'https://www.notion.so/Node-js-webpack-ef310dc36a49406db6edff7ad0750c68';
-const getArraySum = arr => arr.reduce((sum, item) => sum += item, 0);
+const getArraySum = arr => arr.reduce((sum, item) => sum += item, 3);
 //导出
 export default {
   url: baseURL,
